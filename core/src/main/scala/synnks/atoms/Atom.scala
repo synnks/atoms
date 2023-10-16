ce9537355968740ddf44e9f7342be7f28b5e8d42
Daniel Stanila <daniel.stanila@synnks.com>
2023-10-16 18:48:01 +0200

    Introduce GroupedAtoms structure for holding arbitrary levels of nested atoms

--- core/src/main/scala/synnks/atoms/Atom.scala
+++ core/src/main/scala/synnks/atoms/Atom.scala
@@ -5,7 +5,7 @@ import shapeless.*
 final case class Atom[K <: HList, V](keys: K, value: V) {
 
   def map[NK <: HList, NV](f: (K, V) => (NK, NV)): Atom[NK, NV] =
-    (Atom[NK, NV] _).tupled(f(keys, value))
+    Atom.apply[NK, NV].tupled(f(keys, value))
 
   def mapKeys[NK <: HList](f: K => NK): Atom[NK, V] =
     map((keys, value) => (f(keys), value))
--- core/src/main/scala/synnks/atoms/Atoms.scala
+++ core/src/main/scala/synnks/atoms/Atoms.scala
@@ -1,16 +1,34 @@
 package synnks.atoms
 
-import cats.Reducible
-import cats.data.NonEmptyList
+import cats.{ Reducible, Semigroup }
+import cats.data.{ NonEmptyList, NonEmptyMap }
 import cats.syntax.all.*
 import shapeless.*
 import synnks.atoms.ops.GroupBy
 
-final case class Atoms[K <: HList, V](values: NonEmptyList[Atom[K, V]]) {
+sealed trait GroupedAtoms[G <: HList, K <: HList, V] {
 
-  def map[NK <: HList, NV](f: Atom[K, V] => Atom[NK, NV]): Atoms[NK, NV] = Atoms(values.map(f))
+  def ++(other: GroupedAtoms[G, K, V]): GroupedAtoms[G, K, V]
 
-  def mapKeys[NK <: HList](f: K => NK): Atoms[NK, V] = Atoms(values.map(_.mapKeys(f)))
+  def map[NK <: HList, NV](f: Atom[K, V] => Atom[NK, NV]): GroupedAtoms[G, NK, NV]
+
+  def mapKeys[NK <: HList](f: K => NK): GroupedAtoms[G, NK, V] = map(_.mapKeys(f))
+}
+
+object GroupedAtoms {
+
+  implicit def semigroup[G <: HList, K <: HList, V]: Semigroup[GroupedAtoms[G, K, V]] = _ ++ _
+}
+
+final case class Atoms[K <: HList, V](values: NonEmptyList[Atom[K, V]]) extends GroupedAtoms[HNil, K, V] {
+
+  override def ++(other: GroupedAtoms[HNil, K, V]): Atoms[K, V] = other match {
+    case Atoms(values) => Atoms(this.values ::: values)
+  }
+
+  override def map[NK <: HList, NV](f: Atom[K, V] => Atom[NK, NV]): Atoms[NK, NV] = Atoms(values.map(f))
+
+  override def mapKeys[NK <: HList](f: K => NK): Atoms[NK, V] = Atoms(values.map(_.mapKeys(f)))
 
   def groupBy[L <: HList](implicit groupBy: GroupBy[L, K, V]): groupBy.Out = groupBy(this)
 }
@@ -18,5 +36,17 @@ final case class Atoms[K <: HList, V](values: NonEmptyList[Atom[K, V]]) {
 object Atoms {
 
   def apply[C[_]: Reducible, K <: HList, V](values: C[(K, V)]): Atoms[K, V] =
-    Atoms(values.toNonEmptyList.map { case (keys, value) => Atom(keys, value) })
+    Atoms(values.toNonEmptyList.map(Atom.apply[K, V].tupled))
+}
+
+final private[atoms] case class NestedAtoms[GH, GT <: HList, K <: HList, V](
+  groupedAtoms: NonEmptyMap[GH, GroupedAtoms[GT, K, V]]
+) extends GroupedAtoms[GH :: GT, K, V] {
+
+  override def ++(other: GroupedAtoms[GH :: GT, K, V]): GroupedAtoms[GH :: GT, K, V] = other match {
+    case NestedAtoms(groupedAtoms) => NestedAtoms(this.groupedAtoms |+| groupedAtoms)
+  }
+
+  override def map[NK <: HList, NV](f: Atom[K, V] => Atom[NK, NV]): GroupedAtoms[GH :: GT, NK, NV] =
+    NestedAtoms(groupedAtoms.map(_.map(f)))
 }
