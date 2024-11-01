64a7561ed672aa2858b9584aafda6fc9b253889f
Daniel Stanila <daniel.stanila@synnks.com>
2024-11-01 15:43:21 +0100

    Introduce next KH element for Init chained map reduce function to distinguish shape from Last

--- core/src/main/scala/synnks/atoms/mapreduce/ChainedMapReduceFunction.scala
+++ core/src/main/scala/synnks/atoms/mapreduce/ChainedMapReduceFunction.scala
@@ -9,8 +9,8 @@ object ChainedMapReduceFunction {
   case class Last[KH, VH, `VH+1`](mapReduceFn: MapReduceFunction[KH, `VH+1`, VH])
       extends ChainedMapReduceFunction[KH :: HNil, VH :: `VH+1` :: HNil]
 
-  case class Init[KH, KT <: HList, VH, `VH+1`, VT <: HList](
+  case class Init[KH, `KH+1`, KT <: HList, VH, `VH+1`, VT <: HList](
     mapReduceFn: MapReduceFunction[KH, `VH+1`, VH],
-    next: ChainedMapReduceFunction[KT, `VH+1` :: VT]
-  ) extends ChainedMapReduceFunction[KH :: KT, VH :: `VH+1` :: VT]
+    next: ChainedMapReduceFunction[`KH+1` :: KT, `VH+1` :: VT]
+  ) extends ChainedMapReduceFunction[KH :: `KH+1` :: KT, VH :: `VH+1` :: VT]
 }
