1887900445709f2305019780ce6308a0b2ec90eb
Daniel Stanila <daniel.stanila@synnks.com>
2026-06-08 23:10:20 +0200

    Replace eta-expansion applies

--- core/src/main/scala/synnks/atoms/Atom.scala
+++ core/src/main/scala/synnks/atoms/Atom.scala
@@ -4,8 +4,10 @@ import synnks.atoms.hlist.*
 
 final case class Atom[K <: HList, V](keys: K, value: V) {
 
-  def map[NK <: HList, NV](f: (K, V) => (NK, NV)): Atom[NK, NV] =
-    (Atom.apply[NK, NV] _).tupled(f(keys, value))
+  def map[NK <: HList, NV](f: (K, V) => (NK, NV)): Atom[NK, NV] = {
+    val (newKeys, newValue) = f(keys, value)
+    Atom(newKeys, newValue)
+  }
 
   def mapKeys[NK <: HList](f: K => NK): Atom[NK, V] =
     map((keys, value) => (f(keys), value))
--- core/src/main/scala/synnks/atoms/ops/Lookup.scala
+++ core/src/main/scala/synnks/atoms/ops/Lookup.scala
@@ -29,7 +29,7 @@ object Lookup {
       override type Out = ungroupBy.Out
 
       override def apply(groupedAtoms: GroupedAtoms[G, K, V], lookupKeys: HNil): Option[Out] =
-        Some(groupedAtoms.ungroupBy[G](ungroupBy))
+        Some(ungroupBy(groupedAtoms))
     }
 
   implicit def lookupSameHead[LH, LT <: HList, GT <: HList, K <: HList, V](implicit
