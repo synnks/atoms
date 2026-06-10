7a2aa02c1571fed905b78b989445234aea2c7fd0
Daniel Stanila <daniel.stanila@synnks.com>
2026-06-10 20:54:14 +0200

    Remove unused HList prepend syntax

--- hlist/src/main/scala-2.13/synnks/atoms/hlist/ops/hlist/package.scala
+++ hlist/src/main/scala-2.13/synnks/atoms/hlist/ops/hlist/package.scala
@@ -1,9 +1,6 @@
 package synnks.atoms.hlist.ops
 
 package object hlist {
-  type Prepend[L <: shapeless.HList, R <: shapeless.HList] = shapeless.ops.hlist.Prepend[L, R]
-  val Prepend: shapeless.ops.hlist.Prepend.type = shapeless.ops.hlist.Prepend
-
   type Remove[L <: shapeless.HList, E] = shapeless.ops.hlist.Remove[L, E]
   val Remove: shapeless.ops.hlist.Remove.type = shapeless.ops.hlist.Remove
 }
